33956a1f4e0874d66f237227983da8121cd1d5f3
Amitkumar Padole <AM20541160@wipro.com>
2026-02-09 14:23:01 +0530

    add unit to unit conversion

--- Bridgelabzproject/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ Bridgelabzproject/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,186 +1,110 @@
 package com.apps.quantitymeasurement;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static com.apps.quantitymeasurement.Length.convert;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class QuantityMeasurementAppTest {
 
-    // 1
     @Test
-    public void testFeetEquality_YardToYard_SameValue(){
-        Length length1 = new Length(1.0, Length.LengthUnit.YARD);
-        Length length2 = new Length(1.0, Length.LengthUnit.YARD);
-        boolean expectedResult = true;
-        boolean actualResult = length1.equals(length2);
+    public void testConversion_FeetToInches() throws MyException {
+        double actualResult = convert(1.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES);
+        double expectedResult = 12.0;
         assertEquals(expectedResult, actualResult);
     }
 
-    // 2
     @Test
-    public void testFeetEquality_YardToYard_DifferentValue(){
-        Length length1 = new Length(1.0, Length.LengthUnit.YARD);
-        Length length2 = new Length(2.0, Length.LengthUnit.YARD);
-        boolean expectedResult = false;
-        boolean actualResult = length1.equals(length2);
+    public void testConvertestConversion_InchesToFeetsion_FeetToInches() throws MyException {
+        double actualResult = convert(24.0, Length.LengthUnit.INCHES, Length.LengthUnit.FEET);
+        double expectedResult = 2.0;
         assertEquals(expectedResult, actualResult);
     }
 
     // 3
     @Test
-    public void testEquality_YardToFeet_EquivalentValue(){
-        Length length1 = new Length(1.0, Length.LengthUnit.YARD);
-        Length length2 = new Length(3.0, Length.LengthUnit.FEET);
-        boolean expectedResult = true;
-        boolean actualResult = length1.equals(length2);
+    public void testConversion_YardsToInches() throws MyException {
+        double actualResult = convert(1.0, Length.LengthUnit.YARD, Length.LengthUnit.INCHES);
+        double expectedResult = 36.0;
         assertEquals(expectedResult, actualResult);
     }
 
-    //4
+    // 4
     @Test
-    public void testEquality_FeetToYard_EquivalentValue(){
-        Length length1 = new Length(3.0, Length.LengthUnit.FEET);
-        Length length2 = new Length(1.0, Length.LengthUnit.YARD);
-        boolean expectedResult = true;
-        boolean actualResult = length1.equals(length2);
+    public void testConversion_InchesToYards() throws MyException {
+        double actualResult = convert(72.0, Length.LengthUnit.INCHES, Length.LengthUnit.YARD);
+        double expectedResult = 2.0;
         assertEquals(expectedResult, actualResult);
     }
 
-    //5
+    // 5
     @Test
-    public void testEquality_YardToInches_EquivalentValue(){
-        Length length1 = new Length(1.0, Length.LengthUnit.YARD);
-        Length length2 = new Length(36.0, Length.LengthUnit.INCHES);
-        boolean expectedResult = true;
-        boolean actualResult = length1.equals(length2);
+    public void testConversion_CentimetersToInches() throws MyException {
+        double actualResult = Length.convert(2.54, Length.LengthUnit.CM, Length.LengthUnit.INCHES);
+        double expectedResult = 1.0;
         assertEquals(expectedResult, actualResult);
     }
 
-    //6
+    // 6
     @Test
-    public void testEquality_InchesToYard_EquivalentValue(){
-        Length length1 = new Length(36.0, Length.LengthUnit.INCHES);
-        Length length2 = new Length(1.0, Length.LengthUnit.YARD);
-        boolean expectedResult = true;
-        boolean actualResult = length1.equals(length2);
+    public void testConversion_FeatToYard() throws MyException {
+        double actualResult = convert(6.0, Length.LengthUnit.FEET, Length.LengthUnit.YARD);
+        double expectedResult = 2.0;
         assertEquals(expectedResult, actualResult);
     }
 
-    //7
+    // 7
     @Test
-    public void testEquality_YardToFeet_NonEquivalentValue(){
-        Length length1 = new Length(1.0, Length.LengthUnit.YARD);
-        Length length2 = new Length(2.0, Length.LengthUnit.FEET);
-        boolean expectedResult = false;
-        boolean actualResult = length1.equals(length2);
+    public void testConversion_RoundTrip_PreservesValue() throws MyException {
+        // convert(convert(v, A, B), B, A) ≈ v
+        double expectedResult = 1.0;
+        double actualResult = convert(convert(1.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES),Length.LengthUnit.INCHES, Length.LengthUnit.FEET);
         assertEquals(expectedResult, actualResult);
     }
 
-    //8
+    // 8
     @Test
-    public void testEquality_centimetersToInches_EquivalentValue(){
-        Length length1 = new Length(1.0, Length.LengthUnit.CM);
-        Length length2 = new Length(0.393701, Length.LengthUnit.INCHES);
-        boolean expectedResult = true;
-        boolean actualResult = length1.equals(length2);
+    public void testConversion_ZeroValue() throws MyException {
+        double actualResult = convert(0.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES);
+        double expectedResult = 0.0;
         assertEquals(expectedResult, actualResult);
     }
 
-    //9
+    // 9
     @Test
-    public void testEquality_centimetersToFeet_NonEquivalentValue(){
-        Length length1 = new Length(1.0, Length.LengthUnit.CM);
-        Length length2 = new Length(1.0, Length.LengthUnit.FEET);
-        boolean expectedResult = false;
-        boolean actualResult = length1.equals(length2);
+    public void testConversion_NegativeValue() throws MyException {
+        double actualResult = convert(-1.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES);
+        double expectedResult = -12;
         assertEquals(expectedResult, actualResult);
     }
 
     // 10
     @Test
-    public void testEquality_MultiUnit_TransitiveProperty(){
-
-        Length length1 = new Length(1.0, Length.LengthUnit.YARD);
-        Length length2 = new Length(3.0, Length.LengthUnit.FEET);
-        Length length3 = new Length(36.0, Length.LengthUnit.INCHES);
-
-        assertAll(
-                () ->   assertEquals(true, length1.equals(length2)),
-                () ->   assertEquals(true, length2.equals(length3)),
-                () ->   assertEquals(true, length1.equals(length3))
-        );
+    public void testConversion_InvalidUnit_Throws() throws MyException {
+        MyException exception = assertThrows(MyException.class, () -> {
+            convert(1.0, Length.LengthUnit.FEET, null);
+        });
+        boolean result = exception.getMessage().equals("Please enter source and target");
+        assertEquals(true, result);
     }
 
-    //11
+    // 11
     @Test
-    public void testEquality_YardWithNullUnit(){
-        Length length1 = new Length(1.0, Length.LengthUnit.YARD);
-        Length length2 = new Length(1.0, null);
-        boolean expectedResult = false;
-        boolean actualResult = length1.equals(length2);
-        assertEquals(expectedResult, actualResult);
+    public void testConversion_NaNOrInfinite_Throws() throws MyException {
+        MyException exception = assertThrows(MyException.class, () -> {
+            convert(Double.POSITIVE_INFINITY, Length.LengthUnit.FEET, Length.LengthUnit.INCHES);
+        });
+        boolean result = exception.getMessage().equals("Please enter a finite value");
+        assertEquals(true, result);
     }
 
-    //12
+    // 12
     @Test
-    public void testEquality_YardSameReference(){
-        Length length1 = new Length(1.0, Length.LengthUnit.CM);
-        Length length2 = length1;
-        boolean expectedResult = true;
-        boolean actualResult = length1.equals(length2);
-        assertEquals(expectedResult, actualResult);
-    }
-
-    //13
-    @Test
-    public void testEquality_YardNullComparison(){
-        Length length1 = new Length(1.0, Length.LengthUnit.YARD);
-        Length length2 = null;
-        boolean expectedResult = false;
-        boolean actualResult = length1.equals(length2);
-        assertEquals(expectedResult, actualResult);
-    }
-
-    //14
-    @Test
-    public void testEquality_CentimetersWithNullUnit(){
-        Length length1 = new Length(1.0, Length.LengthUnit.CM);
-        Length length2 = new Length(1.0, null);
-        boolean expectedResult = false;
-        boolean actualResult = length1.equals(length2);
-        assertEquals(expectedResult, actualResult);
-    }
-
-    //15
-    @Test
-    public void testEquality_CentimetersSameReference(){
-        Length length1 = new Length(1.0, Length.LengthUnit.CM);
-        Length length2 = length1;
-        boolean expectedResult = true;
-        boolean actualResult = length1.equals(length2);
-        assertEquals(expectedResult, actualResult);
-    }
-
-    //16
-    @Test
-    public void testEquality_CentimetersNullComparison(){
-        Length length1 = new Length(1.0, Length.LengthUnit.CM);
-        Length length2 = null;
-        boolean expectedResult = false;
-        boolean actualResult = length1.equals(length2);
-        assertEquals(expectedResult, actualResult);
-    }
-
-    //17
-    @Test
-    public void testEquality_AllUnits_ComplexScenario(){
-        Length length1 = new Length(1.0, Length.LengthUnit.YARD);
-        Length length2 = new Length(3.0, Length.LengthUnit.FEET);
-        Length length3 = new Length(36.0, Length.LengthUnit.INCHES);
-
-        assertAll(
-                () ->   assertEquals(true, length1.equals(length2)),
-                () ->   assertEquals(true, length2.equals(length3)),
-                () ->   assertEquals(true, length1.equals(length3))
-        );
+    public void testConversion_PrecisionTolerance() throws MyException {
+        MyException exception = assertThrows(MyException.class, () -> {
+            convert(1.0, Length.LengthUnit.FEET, null);
+        });
+        boolean result = exception.getMessage().equals("Please enter source and target");
+        assertEquals(true, result);
     }
 }
